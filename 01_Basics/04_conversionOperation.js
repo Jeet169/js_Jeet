ee02f04cd5e42fabfa16eab2a25d9ada3b381949
Jeet169 <125563268+Jeet169@users.noreply.github.com>
2025-06-12 09:05:47 +0000

    conversionOperation

---
+++ 01_Basics/04_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33as";
+// let score = true;
+// let score = null;
+let score = undefined;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1   false => 0 
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+let strNum = "4857sd"
+
+let temp = String(strNum);
+console.log(typeof temp);
+console.log(temp);
