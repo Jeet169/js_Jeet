54415671b38fbc8089837bc5daa93bafe94e42eb
Jeet169 <125563268+Jeet169@users.noreply.github.com>
2025-06-12 08:35:09 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,8 @@
+"use strict"
+
+// alert(3+3);
+
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof Symbol);
+
---
+++ 01_Basics/javaScriptVar.js
@@ -0,0 +1,13 @@
+const accountId = 11223344
+let accountEmail = 'jiteber@gmail.com'
+var accountPassword = '643523'
+accountCity = 'Bulandshar'
+let accountState;
+
+// accountId = 3456748
+// console.log(accountId);
+accountEmail = 'hdbch@gamil.com'
+accountPassword = '765473'
+accountCity = 'Roorkee'
+
+console.log([accountId , accountEmail , accountPassword , accountCity , accountState]);
