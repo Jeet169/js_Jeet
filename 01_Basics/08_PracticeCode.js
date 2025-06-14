a68dad78cd010de393340924231e247e79f7be9c
Jeet169 <125563268+Jeet169@users.noreply.github.com>
2025-06-14 07:41:15 +0000

    PracticeCode

---
+++ 01_Basics/08_PracticeCode.js
@@ -0,0 +1,30 @@
+let var1 = "this is the fantestic boy";
+// console.log(typeof var1);
+
+let var2 = new String("tempo");
+// console.log(typeof var2);
+if(var2 === "tempo"){
+    console.log('true');
+}else{
+    console.log('false');
+    console.log('------->' + var2)
+}
+
+let var3 = String("kjfbre")
+// console.log(typeof var3);
+
+if(var3 === "kjfbre"){
+    // console.log('true');
+    // console.log('------->' + var3)
+}else{
+    console.log('false');
+}
+
+
+let obj1 = {
+    one : "value1",
+    two : "value2",
+    three: "value3"
+}
+
+console.log(obj1);
