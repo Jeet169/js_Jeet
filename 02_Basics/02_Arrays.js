dcf31a45e6f0e62e9eb643fe824441d302eaed77
Jeet169 <125563268+Jeet169@users.noreply.github.com>
2025-06-16 14:14:03 +0000

    Arrays

---
+++ 02_Basics/02_Arrays.js
@@ -0,0 +1,12 @@
+const marvelheroes = ['Batsman' , 'Superman' , 'IronMan'];
+const dcHeroes = ['Hulk' , 'SpiderMan' , 'Roger'];
+// console.log(marvelheroes);
+// marvelheroes.push(...dcHeroes);
+
+// const allHeroes = [...marvelheroes,...dcHeroes];
+const allHeroes = marvelheroes.concat(dcHeroes);
+// console.log(marvelheroes[3][0]);
+console.log(allHeroes);
+
+
+
