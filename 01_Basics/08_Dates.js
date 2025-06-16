dec8fd6929652253441964f59782ae07e53d4bfa
Jeet169 <125563268+Jeet169@users.noreply.github.com>
2025-06-16 13:00:40 +0000

    Dates

---
+++ 01_Basics/08_Dates.js
@@ -0,0 +1,11 @@
+let myDate  = new Date();
+/* console.log(`--------to String--->`,myDate.toString());
+console.log(`-------toISOString---->`,myDate.toISOString());
+console.log(`---------toJSON--------->`,myDate.toJSON());
+console.log(`----------toLocaleDateString------->`,myDate.toLocaleDateString());
+console.log(`--------toLocaleString------->`,myDate.toLocaleString());
+console.log(`--------toLocaleTimeString------>`,myDate.toLocaleTimeString()); 
+console.log(`------> ` , typeof myDate);*/
+
+let timeStamp = Date.now();
+console.log(timeStamp.toString());
