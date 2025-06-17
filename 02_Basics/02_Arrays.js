2d0b03a7b7561b9cdf78d8b8174a91ea0080ea54
Jeet169 <125563268+Jeet169@users.noreply.github.com>
2025-06-17 07:32:59 +0000

    Arrays

--- 02_Basics/02_Arrays.js
+++ 02_Basics/02_Arrays.js
@@ -6,7 +6,24 @@ const dcHeroes = ['Hulk' , 'SpiderMan' , 'Roger'];
 // const allHeroes = [...marvelheroes,...dcHeroes];
 const allHeroes = marvelheroes.concat(dcHeroes);
 // console.log(marvelheroes[3][0]);
-console.log(allHeroes);
+// console.log(allHeroes);
+
+// const another_array = [1,2,3,[4,5,6],7,8,[10 , 11,[12,13,14]]];
+// let realAnotherArray = another_array.flat(3);
+// console.log(another_array);
+// console.log(realAnotherArray);
+
+/* console.log(Array.isArray("jitendra"));
+console.log(Array.from("jitendra"));
+console.log(Array.from({name: "jitendra"})); */
+
+let score1 = 23
+let score2 = 25
+let score3 = 37
+
+console.log(Array.of(score1 , score2 , score3));
+
+
 
 
 
