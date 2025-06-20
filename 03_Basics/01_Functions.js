0f0be458a4093e1f0c12d5a3840ce7cabcb26e26
Jeet169 <125563268+Jeet169@users.noreply.github.com>
2025-06-20 08:22:05 +0000

    Functions

---
+++ 03_Basics/01_Functions.js
@@ -0,0 +1,31 @@
+function getAmount(...amount) {
+    return amount
+}
+
+// console.log(getAmount(67578 , 898 , 897));
+
+const personInfo = {
+    firstname : "jitendra",
+    lastname : "kumar",
+    age : 27,
+    religion : "Hindu"
+}
+
+const classInfo = [44 , 432 , 3483, 'Ravi' , 'Shweta' , 'pallavi' , 'Sulekha'];
+
+function personDetails(anyObject) {
+    return anyObject
+}
+
+console.log(personDetails(classInfo[0]));
+// console.log(personDetails([44 , 432 , 3483, 'Ravi' , 'Shweta' , 'pallavi' , 'Sulekha']));
+
+// console.log(personDetails(personInfo.age));
+/* console.log(personDetails({
+    firstname : "jitendra",
+    lastname : "kumar",
+    age : 27,
+    religion : "Hindu"
+})); */
+
+
